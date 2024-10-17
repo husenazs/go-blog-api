569d8c30b7f74f13f91e754ea59cf96791f1701a
Hoosen <husenazis.ha@gmail.com>
2024-10-17 09:45:41 +0700

    update UI and add editorConfig

--- cmd/main.go
+++ cmd/main.go
@@ -2,29 +2,77 @@ package main
 
 import (
 	"blog-api/internal/handler"
+	"blog-api/internal/models"
 	"blog-api/internal/service"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
+func formattedTime(timeStr string) string {
+	// Format datetime sesuai kebutuhan
+	layout := "2006-01-02 15:04:05"
+	t, err := time.Parse(layout, timeStr)
+	if err != nil {
+		log.Println("Error parsing time:", err)
+		return timeStr
+	}
+	formattedTime := fmt.Sprintf("%d-%s-%d", t.Day(), t.Month().String(), t.Year())
+
+	return formattedTime
+}
+
+func randNumber() string {
+	return fmt.Sprintf("%d", time.Now().Day())
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, err := r.Cookie("token")
+		// Call API untuk mendapatkan posts
+		resp, err := http.Get("http://localhost:8080/posts")
 		if err != nil {
-			http.Redirect(w, r, "/login", http.StatusFound)
+			log.Println("Error getting posts:", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+
+		var posts []models.Post
+		// Decode response body into slice of posts
+		if err := json.NewDecoder(resp.Body).Decode(&posts); err != nil {
+			log.Println("Error decoding posts:", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			return
 		}
 
+		// Loop through the posts and update the Created_at and Updated_at fields
+		for i, post := range posts {
+			posts[i].Created_at = formattedTime(post.Created_at)
+			posts[i].Updated_at = formattedTime(post.Updated_at)
+			posts[i].Post_ID = randNumber()
+		}
+
+		// Parsing template dan passing data posts
 		t, err := template.ParseFiles("../static/template/index.html")
 		if err != nil {
-			log.Fatal("Error parsing template:", err)
+			log.Println("Error parsing template:", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
 		}
-		t.Execute(w, nil)
+
+		// Eksekusi template dengan data posts
+		if err := t.Execute(w, struct {
+			Posts []models.Post
+		}{Posts: posts}); err != nil {
+			log.Println("Error executing template:", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		}
 	}).Methods("GET")
 
 	r.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
@@ -51,8 +99,8 @@ func main() {
 	r.Handle("/register", http.HandlerFunc(service.Register)).Methods("POST")
 
 	// Menggunakan http.HandlerFunc untuk mengkonversi handler ke http.Handler
-	r.Handle("/posts", service.AuthMiddleware(http.HandlerFunc(handler.GetPost))).Methods("GET")
-	r.Handle("/posts/{id}", service.AuthMiddleware(http.HandlerFunc(handler.GetPostbyID))).Methods("GET")
+	r.Handle("/posts", http.HandlerFunc(handler.GetPost)).Methods("GET")
+	r.Handle("/posts/{id}", http.HandlerFunc(handler.GetPostbyID)).Methods("GET")
 	r.Handle("/posts/create", service.AuthMiddleware(http.HandlerFunc(handler.CreatePost))).Methods("POST")
 	r.Handle("/posts/update/{id}", service.AuthMiddleware(http.HandlerFunc(handler.UpdatePost))).Methods("PUT")
 	fmt.Println("Server started on port 8080")
